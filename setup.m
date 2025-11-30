9caceb1d81261914c0b8d89841ea05295584c8d1
DLikelyUni <duncan.likely.2021@uni.strath.ac.uk>
2025-11-30 16:58:02 +0000

    added test images

--- setup.m
+++ setup.m
@@ -1,2 +1,11 @@
 %% OBJ DETECTION SETUP SCRIPT
 %% Duncan Likely
+
+filepath = ".\test_imgs\";
+
+images = filepath + ["IMG_9415.jpg" "IMG_9416.jpg" "IMG_9417.jpg"];
+
+
+img1 = imread(images(1));
+
+input_image = img1;
